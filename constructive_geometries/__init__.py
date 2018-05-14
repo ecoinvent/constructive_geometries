de401bf9f7da778e4219e4afa0f9292118c0be5d
Chris Mutel <cmutel@gmail.com>
2018-05-14 15:02:27 +0200

    0.6.2

--- constructive_geometries/__init__.py
+++ constructive_geometries/__init__.py
@@ -1,4 +1,4 @@
-__version__ = (0, 6, 1, 3)
+__version__ = (0, 6, 2)
 
 
 from .cg import ConstructiveGeometries
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 setup(
     name='constructive_geometries',
-    version="0.6.1.3",
+    version="0.6.2",
     packages=["constructive_geometries"],
     package_data={'constructive_geometries': [
         "data/faces.gpkg",
